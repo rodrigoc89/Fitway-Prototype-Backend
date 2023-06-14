764812035fec49532417c22b5f5a317d6af761c7
Rodrigo Castellanos <rodrigojoaquinc89@gmail..com>
2023-06-14 12:49:22 -0300

    update regex

--- middleware/passwordStrong.js
+++ middleware/passwordStrong.js
@@ -1,5 +1,5 @@
 const validatePassword = (password) => {
-  const regex = /^(?=.*[a-z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,20}$/;
+  const regex = /^(?=.*\d)(?=.*[^\da-zA-Z]).{8,20}$/;
   return regex.test(password);
 };
 
@@ -10,8 +10,6 @@ const passwordValidator = (req, res, next) => {
     return res.status(400).json({
       error:
         "Password must contain at least one lowercase letter, one uppercase letter, one number, one symbol, and a minimum of 8 characters and a maximum of 20.",
-      // message: "There was a problem register the user",
-      // details: error.message,
     });
   }
 
