042442a505af3a5cce78fd4e2e2cc565f89ba8b7
Mateo Bertello <mateo.bertello@gmail.com>
2023-07-17 08:52:34 -0300

    Descomentando 2

--- routes/superset.js
+++ routes/superset.js
@@ -28,263 +28,263 @@ router.get("/", async (req, res) => {
   }
 });
 
-// router.get("/:supersetId", async (req, res) => {
-//   const { supersetId } = req.params;
-//   try {
-//     const superSet = await SuperSet.findByPk(supersetId, {
-//       include: {
-//         model: Exercise,
-//         through: { attributes: [] },
-//       },
-//     });
+ router.get("/:supersetId", async (req, res) => {
+   const { supersetId } = req.params;
+   try {
+     const superSet = await SuperSet.findByPk(supersetId, {
+       include: {
+         model: Exercise,
+         through: { attributes: [] },
+       },
+     });
 
-//     if (!superSet) {
-//       return res.status(404).json({ message: "super set not found" });
-//     }
+     if (!superSet) {
+       return res.status(404).json({ message: "super set not found" });
+     }
 
-//     res.status(200).send(superSet);
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem finding the super set",
-//       details: error.message,
-//     });
-//   }
-// });
+     res.status(200).send(superSet);
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem finding the super set",
+       details: error.message,
+     });
+   }
+ });
 
 router.get("/:supersetId", superSetController.getSuperSet);
 
-// router.get("/getExercises/:supersetId", async (req, res) => {
-//   const { supersetId } = req.params;
-//   try {
-//     const superSet = await SuperSet.findByPk(supersetId);
-//     const exercises = await superSet.getExercises();
-//     res.status(200).send(exercises);
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem finding exercises into the super set",
-//       details: error.message,
-//     });
-//   }
-// });
+ router.get("/getExercises/:supersetId", async (req, res) => {
+   const { supersetId } = req.params;
+   try {
+     const superSet = await SuperSet.findByPk(supersetId);
+     const exercises = await superSet.getExercises();
+     res.status(200).send(exercises);
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem finding exercises into the super set",
+       details: error.message,
+     });
+   }
+ });
 
 router.get("/getExercises/:supersetId", superSetController.getExercises);
 
-// router.post("/newSuperSet/:userId/:routineId", async (req, res) => {
-//   const { routineId, userId } = req.params;
-//   try {
-//     const { order, quantity } = req.body;
+ router.post("/newSuperSet/:userId/:routineId", async (req, res) => {
+   const { routineId, userId } = req.params;
+   try {
+     const { order, quantity } = req.body;
 
-//     const user = await User.findByPk(userId);
+     const user = await User.findByPk(userId);
 
-//     if (!user) {
-//       return res.status(404).json({ message: "user not found" });
-//     }
+     if (!user) {
+       return res.status(404).json({ message: "user not found" });
+     }
 
-//     const routine = await Routine.findByPk(routineId);
+     const routine = await Routine.findByPk(routineId);
 
-//     if (!routine) {
-//       return res.status(404).json({ message: "routine not found" });
-//     }
+     if (!routine) {
+       return res.status(404).json({ message: "routine not found" });
+     }
 
-//     const newSuperset = await SuperSet.create({
-//       UserId: userId,
-//       order,
-//       quantity,
-//     });
+     const newSuperset = await SuperSet.create({
+       UserId: userId,
+       order,
+       quantity,
+     });
 
-//     await routine.addSuperSet(newSuperset);
+     await routine.addSuperSet(newSuperset);
 
-//     res.status(201).send(newSuperset);
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem creating super set",
-//       details: error.message,
-//     });
-//   }
-// });
+     res.status(201).send(newSuperset);
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem creating super set",
+       details: error.message,
+     });
+   }
+ });
 
 router.post("/newSuperSet/:userId/:routineId", superSetController.newSuperSet);
 
-// router.post("/addSuperSet/:superSetId/:routineId", async (req, res) => {
-//   const { superSetId, routineId } = req.params;
-//   try {
-//     const superSet = await SuperSet.findByPk(superSetId);
+ router.post("/addSuperSet/:superSetId/:routineId", async (req, res) => {
+   const { superSetId, routineId } = req.params;
+   try {
+     const superSet = await SuperSet.findByPk(superSetId);
 
-//     if (!superSet) {
-//       return res.status(404).json({ message: "super set not found" });
-//     }
+     if (!superSet) {
+       return res.status(404).json({ message: "super set not found" });
+     }
 
-//     const routine = await Routine.findByPk(routineId);
+     const routine = await Routine.findByPk(routineId);
 
-//     if (!routine) {
-//       return res.status(404).json({ message: "routine not found" });
-//     }
+     if (!routine) {
+       return res.status(404).json({ message: "routine not found" });
+     }
 
-//     await routine.addSuperSet(superSet);
+     await routine.addSuperSet(superSet);
 
-//     res.status(200).send(superSet);
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem adding the super set",
-//       details: error.message,
-//     });
-//   }
-// });
+     res.status(200).send(superSet);
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem adding the super set",
+       details: error.message,
+     });
+   }
+ });
 router.post(
   "/addSuperSet/:superSetId/:routineId",
   superSetController.addSuperSet
 );
 
-// router.delete(
-//   "/removeExercise/:routineId/:superSetId/:exerciseId",
-//   async (req, res) => {
-//     const { exerciseId, superSetId, routineId } = req.params;
+ router.delete(
+   "/removeExercise/:routineId/:superSetId/:exerciseId",
+   async (req, res) => {
+     const { exerciseId, superSetId, routineId } = req.params;
 
-//     try {
-//       const [routine, superSet, exercise] = await Promise.all([
-//         Routine.findByPk(routineId),
-//         SuperSet.findByPk(superSetId),
-//         Exercise.findByPk(exerciseId),
-//       ]);
+     try {
+       const [routine, superSet, exercise] = await Promise.all([
+         Routine.findByPk(routineId),
+         SuperSet.findByPk(superSetId),
+         Exercise.findByPk(exerciseId),
+       ]);
 
-//       if (!routine) {
-//         return res.status(404).json({ message: "routine not found" });
-//       }
-//       if (!superSet) {
-//         return res.status(404).json({ message: "superset not found" });
-//       }
-//       if (!exercise) {
-//         return res.status(404).json({ message: "exercise not found" });
-//       }
+       if (!routine) {
+         return res.status(404).json({ message: "routine not found" });
+       }
+       if (!superSet) {
+         return res.status(404).json({ message: "superset not found" });
+       }
+       if (!exercise) {
+         return res.status(404).json({ message: "exercise not found" });
+       }
 
-//       await superSet.removeExercises(exercise);
+       await superSet.removeExercises(exercise);
 
-//       const muscle = exercise.muscle;
+       const muscle = exercise.muscle;
 
-//       const exerciseCountInRoutine = await routine.countExercises({
-//         where: {
-//           muscle: exercise.muscle,
-//         },
-//       });
+       const exerciseCountInRoutine = await routine.countExercises({
+         where: {
+           muscle: exercise.muscle,
+         },
+       });
 
-//       const superSets = await routine.getSuperSets({
-//         include: [
-//           {
-//             model: Exercise,
-//           },
-//         ],
-//       });
+       const superSets = await routine.getSuperSets({
+         include: [
+           {
+             model: Exercise,
+           },
+         ],
+       });
 
-//       let exerciseCountInSuperSets = 0;
-//       for (const superset of superSets) {
-//         const exercises = superset.Exercises;
-//         const filteredExercises = exercises.filter((e) => e.muscle === muscle);
-//         exerciseCountInSuperSets += filteredExercises.length;
-//       }
+       let exerciseCountInSuperSets = 0;
+       for (const superset of superSets) {
+         const exercises = superset.Exercises;
+         const filteredExercises = exercises.filter((e) => e.muscle === muscle);
+         exerciseCountInSuperSets += filteredExercises.length;
+       }
 
-//       const totalExerciseCount =
-//         exerciseCountInRoutine + exerciseCountInSuperSets;
+       const totalExerciseCount =
+         exerciseCountInRoutine + exerciseCountInSuperSets;
 
-//       if (totalExerciseCount === 0) {
-//         const tagToRemove = await Tag.findOne({
-//           where: {
-//             tagName: exercise.muscle,
-//           },
-//         });
+       if (totalExerciseCount === 0) {
+         const tagToRemove = await Tag.findOne({
+           where: {
+             tagName: exercise.muscle,
+           },
+         });
 
-//         if (tagToRemove) {
-//           await routine.removeTag(tagToRemove);
-//         }
-//       }
+         if (tagToRemove) {
+           await routine.removeTag(tagToRemove);
+         }
+       }
 
-//       res
-//         .status(200)
-//         .send({ message: "The exercise has been removed from the super set" });
-//     } catch (error) {
-//       res.status(422).send({
-//         error: "Unprocessable Entity",
-//         message: "There was a problem deleting the exercise from the super set",
-//         details: error.message,
-//       });
-//     }
-//   }
-// );
+       res
+         .status(200)
+         .send({ message: "The exercise has been removed from the super set" });
+     } catch (error) {
+       res.status(422).send({
+         error: "Unprocessable Entity",
+         message: "There was a problem deleting the exercise from the super set",
+         details: error.message,
+       });
+     }
+   }
+ );
 
 router.delete(
   "/removeExercise/:routineId/:superSetId/:exerciseId",
   superSetController.deletedExercise
 );
 
-// router.delete("/deleteSuperset/:routineId/:supersetId", async (req, res) => {
-//   const { routineId, supersetId } = req.params;
-//   try {
-//     const routine = await Routine.findByPk(routineId);
-//     const superSet = await SuperSet.findByPk(supersetId);
+ router.delete("/deleteSuperset/:routineId/:supersetId", async (req, res) => {
+   const { routineId, supersetId } = req.params;
+   try {
+     const routine = await Routine.findByPk(routineId);
+     const superSet = await SuperSet.findByPk(supersetId);
 
-//     if (!routine) {
-//       return res.status(404).json({ message: "routine not found" });
-//     }
-//     if (!superSet) {
-//       return res.status(404).json({ message: "superSet not found" });
-//     }
+     if (!routine) {
+       return res.status(404).json({ message: "routine not found" });
+     }
+     if (!superSet) {
+       return res.status(404).json({ message: "superSet not found" });
+     }
 
-//     const exercisesInRoutine = await routine.getExercises();
-//     const exercisesInSuperset = await superSet.getExercises();
-//     const otherSupersets = await routine.getSuperSets({
-//       where: {
-//         id: {
-//           [Op.ne]: supersetId,
-//         },
-//       },
-//       include: [
-//         {
-//           model: Exercise,
-//         },
-//       ],
-//     });
+     const exercisesInRoutine = await routine.getExercises();
+     const exercisesInSuperset = await superSet.getExercises();
+     const otherSupersets = await routine.getSuperSets({
+       where: {
+         id: {
+           [Op.ne]: supersetId,
+         },
+       },
+       include: [
+         {
+           model: Exercise,
+         },
+       ],
+     });
 
-//     const exercisesInOtherSupersets = otherSupersets.flatMap(
-//       (superset) => superset.Exercises
-//     );
+     const exercisesInOtherSupersets = otherSupersets.flatMap(
+       (superset) => superset.Exercises
+     );
 
-//     const otherExercises = [
-//       ...exercisesInRoutine,
-//       ...exercisesInOtherSupersets,
-//     ];
+     const otherExercises = [
+       ...exercisesInRoutine,
+       ...exercisesInOtherSupersets,
+     ];
 
-//     const musclesInSupersetTarget = [
-//       ...new Set(exercisesInSuperset.map((exercise) => exercise.muscle)),
-//     ];
+     const musclesInSupersetTarget = [
+       ...new Set(exercisesInSuperset.map((exercise) => exercise.muscle)),
+     ];
 
-//     for (const muscle of musclesInSupersetTarget) {
-//       const matchingExercises = otherExercises.filter(
-//         (e) => e.muscle === muscle
-//       );
-//       if (matchingExercises.length === 0) {
-//         const tagToRemove = await Tag.findOne({
-//           where: {
-//             tagName: muscle,
-//           },
-//         });
-//         await routine.removeTag(tagToRemove);
-//       }
-//     }
+     for (const muscle of musclesInSupersetTarget) {
+       const matchingExercises = otherExercises.filter(
+         (e) => e.muscle === muscle
+       );
+       if (matchingExercises.length === 0) {
+         const tagToRemove = await Tag.findOne({
+           where: {
+             tagName: muscle,
+           },
+         });
+         await routine.removeTag(tagToRemove);
+       }
+     }
 
-//     await SuperSet.destroy({ where: { id: supersetId } });
+     await SuperSet.destroy({ where: { id: supersetId } });
 
-//     res.status(200).send({ message: "the super set has been removed" });
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem removing the exercise of the super set",
-//       details: error.message,
-//     });
-//   }
-// });
+     res.status(200).send({ message: "the super set has been removed" });
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem removing the exercise of the super set",
+       details: error.message,
+     });
+   }
+ });
 
 router.delete(
   "/deleteSuperset/:routineId/:supersetId",
--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ const router = Router();
 const userController = require("../controllers/userController");
 const { route } = require("./search");
 
-// REQUEST USER INFORMATION
+ //REQUEST USER INFORMATION
 
 router.get("/", async (req, res) => {
     try {
@@ -28,52 +28,52 @@ router.get("/", async (req, res) => {
     }
 });
 
-// router.get("/:userId", async (req, res) => {
-//   const { userId } = req.params;
+ router.get("/:userId", async (req, res) => {
+   const { userId } = req.params;
 
-//   try {
-//     const user = await User.findOne({
-//       where: { id: userId },
-//       attributes: { exclude: ["password", "salt"] },
-//     });
+   try {
+     const user = await User.findOne({
+       where: { id: userId },
+       attributes: { exclude: ["password", "salt"] },
+     });
 
-//     res.status(200).send(user);
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem finding the user",
-//       details: error.message,
-//     });
-//   }
-// });
+     res.status(200).send(user);
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem finding the user",
+       details: error.message,
+     });
+   }
+ });
 router.get("/:userId", userController.getUserById);
 
-// router.get("/data/token", async (req, res) => {
-//   try {
-//     const token = req.headers.authorization.split(" ")[1];
+ router.get("/data/token", async (req, res) => {
+   try {
+     const token = req.headers.authorization.split(" ")[1];
 
-//     const decodedToken = validateToken(token);
+     const decodedToken = validateToken(token);
 
-//     const userId = decodedToken.user;
+     const userId = decodedToken.user;
 
-//     const user = await User.findByPk(userId.id);
+     const user = await User.findByPk(userId.id);
 
-//     if (!user.id) {
-//       return res.status(404).send({ message: "User not found" });
-//     }
+     if (!user.id) {
+       return res.status(404).send({ message: "User not found" });
+     }
 
-//     res.send({
-//       id: user.id,
-//       name: user.name,
-//       lastName: user.lastName,
-//       birthdate: user.birthdate,
-//       country: user.country,
-//       email: user.email,
-//     });
-//   } catch (error) {
-//     res.status(401).send({ message: "Invalid token" });
-//   }
-// });
+     res.send({
+       id: user.id,
+       name: user.name,
+       lastName: user.lastName,
+       birthdate: user.birthdate,
+       country: user.country,
+       email: user.email,
+     });
+   } catch (error) {
+     res.status(401).send({ message: "Invalid token" });
+   }
+ });
 router.get("/data/token", userController.getDataToken);
 
 router.get("/routines/:userId", async (req, res) => {
@@ -110,73 +110,73 @@ router.get("/routines/:userId", async (req, res) => {
 });
 router.get("/routines/:userId", userController.getUserRoutines);
 
-// router.get("/exercises/:userId", async (req, res) => {
-//   const { userId } = req.params;
-//   try {
-//     const exercises = await Exercise.findAll({ where: { UserId: userId } });
-//     if (!exercises) {
-//       return res.status(404).send({ message: "exercises not found" });
-//     }
-//     res.status(200).send(exercises);
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem finding the user exercises",
-//       details: error.message,
-//     });
-//   }
-// });
+ router.get("/exercises/:userId", async (req, res) => {
+   const { userId } = req.params;
+   try {
+     const exercises = await Exercise.findAll({ where: { UserId: userId } });
+     if (!exercises) {
+       return res.status(404).send({ message: "exercises not found" });
+     }
+     res.status(200).send(exercises);
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem finding the user exercises",
+       details: error.message,
+     });
+   }
+ });
 router.get("/exercises/:userId", userController.getUserExercises);
 
-// router.get("/superSets/:userId", async (req, res) => {
-//   const { userId } = req.params;
-//   try {
-//     const superSets = await SuperSet.findAll({
-//       where: { UserId: userId },
-//       include: [
-//         {
-//           model: Exercise,
-//         },
-//       ],
-//     });
-//     if (!superSets) {
-//       return res.status(404).send({ message: "superSets not found" });
-//     }
-//     res.status(200).send(superSets);
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem finding the user superset",
-//       details: error.message,
-//     });
-//   }
-// });
+ router.get("/superSets/:userId", async (req, res) => {
+   const { userId } = req.params;
+   try {
+     const superSets = await SuperSet.findAll({
+       where: { UserId: userId },
+       include: [
+         {
+           model: Exercise,
+         },
+       ],
+     });
+     if (!superSets) {
+       return res.status(404).send({ message: "superSets not found" });
+     }
+     res.status(200).send(superSets);
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem finding the user superset",
+       details: error.message,
+     });
+   }
+ });
 router.get("/superSets/:userId", userController.getUserSuperSets);
 
-// router.patch("/editProfile/:userId", async (req, res) => {
-//   const { userId } = req.params;
-//   try {
-//     const { country, birthdate, lastName, name, username } = req.body;
+ router.patch("/editProfile/:userId", async (req, res) => {
+   const { userId } = req.params;
+   try {
+     const { country, birthdate, lastName, name, username } = req.body;
 
-//     const user = await User.findByPk(userId);
+     const user = await User.findByPk(userId);
 
-//     if (!user) {
-//       return res.status(404).send({ message: "User not found" });
-//     }
+     if (!user) {
+       return res.status(404).send({ message: "User not found" });
+     }
 
-//     await user.update({ country, birthdate, lastName, name, username });
+     await user.update({ country, birthdate, lastName, name, username });
 
-//     res.status(200).send(user);
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem updating the user information",
-//       details: error.message,
-//     });
-//   }
-// });
+     res.status(200).send(user);
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem updating the user information",
+       details: error.message,
+     });
+   }
+ });
 router.patch("/editProfile/:userId", userController.editUserProfile);
-// VALIDATE EMAIL
+ //VALIDATE EMAIL
 router.post("/emailValidate", userController.validateEmail);
 router.post("/emailValidate", async (req, res) => {
     try {
@@ -201,7 +201,7 @@ router.post("/emailValidate", async (req, res) => {
     }
 });
 
-// REGISTER
+ REGISTER
 router.post("/register", userController.register);
 router.post("/register", passwordValidator, async (req, res) => {
     const { name, lastName, birthdate, password, email, country, username } =
@@ -236,50 +236,50 @@ router.post("/register", passwordValidator, async (req, res) => {
     }
 });
 
-//LOGIN
+LOGIN
 router.post("/login", userController.login);
-// router.post("/login", async (req, res) => {
-//   const { userLogin, password } = req.body;
-//   try {
-//     const user = await User.findOne({
-//       where: { [Op.or]: [{ email: userLogin }, { username: userLogin }] },
-//     });
+ router.post("/login", async (req, res) => {
+   const { userLogin, password } = req.body;
+   try {
+     const user = await User.findOne({
+       where: { [Op.or]: [{ email: userLogin }, { username: userLogin }] },
+     });
 
-//     if (!user) {
-//       return res.status(401).send({
-//         error: "Unauthorized",
-//         message: "Invalid user",
-//       });
-//     }
+     if (!user) {
+       return res.status(401).send({
+         error: "Unauthorized",
+         message: "Invalid user",
+       });
+     }
 
-//     const validate = await user.validatePassword(password);
+     const validate = await user.validatePassword(password);
 
-//     if (!validate) {
-//       return res.status(401).send({
-//         error: "Unauthorized",
-//         message: "Invalid password",
-//       });
-//     }
+     if (!validate) {
+       return res.status(401).send({
+         error: "Unauthorized",
+         message: "Invalid password",
+       });
+     }
 
-//     const payload = {
-//       id: user.id,
-//       email: user.email,
-//       username: user.username,
-//     };
+     const payload = {
+       id: user.id,
+       email: user.email,
+       username: user.username,
+     };
 
-//     const token = generateToken(payload);
+     const token = generateToken(payload);
 
-//     res.cookie("token", token).send(token);
-//   } catch (error) {
-//     res.status(422).send({
-//       error: "Unprocessable Entity",
-//       message: "There was a problem login the user",
-//       details: error.message,
-//     });
-//   }
-// });
+     res.cookie("token", token).send(token);
+   } catch (error) {
+     res.status(422).send({
+       error: "Unprocessable Entity",
+       message: "There was a problem login the user",
+       details: error.message,
+     });
+   }
+ });
 
-//LOGOUT
+LOGOUT
 
 router.post("/logout", (req, res) => {
     try {
